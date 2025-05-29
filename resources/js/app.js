d778ec44f6c1056a2b836ee21c1a30fe5e109742
Tech-Emiretus <gideonappoh@gmail.com>
2025-05-29 10:25:02 -0700

    Fix styling

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 import { createApp } from "vue";
 import AppLayout from './Layouts/AppLayout.vue';
 import { router } from './router';
+import '../css/app.css';
 
 const app = createApp(AppLayout);
 
